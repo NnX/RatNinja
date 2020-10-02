0fc30f119da126fc5f74d21ec66b8075460e7ebf
NnX <hentis@ukr.net>
2020-10-03 00:39:09 +0300

    fixed punch sounds during game over and pause, updated unity version to 2020.1.7f1

--- Assets/Scripts/Gamecore/GameController.cs
+++ Assets/Scripts/Gamecore/GameController.cs
@@ -1,65 +1,70 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     [SerializeField] private GameObject slainedWindow;
     [SerializeField] private GameObject pauseWindowPrefab;
+
     public static GameController Instance { get; private set; } = null;
-    
+
     private bool _isGameOnPause = false;
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this.gameObject);
+
+    private void Awake () {
+        if (Instance != null && Instance != this) {
+            Destroy (this.gameObject);
         }
 
         Instance = this;
-        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad (this.gameObject);
     }
 
-    public void LoadLevel(int level)
-    {
-        LevelController.Instance.LoadLevel(level);
+    public bool GameStoped () {
+        return _isGameOnPause;
     }
 
-    public void RestartGame()
-    {
-        ResumeGame();
-        LevelController.Instance.RestartGame();
+    public void LoadLevel (int level) {
+        ResumeGame ();
+        LevelController.Instance.LoadLevel (level);
     }
 
-    public void ToWorldMap() {
-        LevelController.Instance.LoadLevel(0);
+    public void RestartGame () {
+        ResumeGame ();
+        LevelController.Instance.RestartGame ();
     }
 
-    public void OnPauseButtonCick() {
-        if(_isGameOnPause) {
-            _isGameOnPause = false;
-            ResumeGame();
+    public void ToWorldMap () {
+        LevelController.Instance.LoadLevel (0);
+    }
+
+    public void OnPauseButtonCick () {
+        if (_isGameOnPause) {
+            ResumeGame ();
         } else {
-            _isGameOnPause = true;
-            PauseGame();
+            PauseGame ();
         }
     }
-    public void PauseGame() {
+
+    public void PauseGame () {
+        _isGameOnPause = true;
         Time.timeScale = 0f;
     }
 
-    public void ResumeGame() {
+    public void ResumeGame () {
+        _isGameOnPause = false;
         Time.timeScale = 1f;
     }
 
-    public void ShowGameOverWindow() {
-        var slaineWindow = Instantiate(slainedWindow, new Vector3(0, 0, 0), Quaternion.identity);
-        var canvas = FindObjectOfType<Canvas>();
-        slaineWindow.transform.SetParent(canvas.transform, false);
+    public void ShowGameOverWindow () {
+        _isGameOnPause = true;
+        var slaineWindow = Instantiate (slainedWindow, new Vector3 (0, 0, 0), Quaternion.identity);
+        var canvas = FindObjectOfType<Canvas> ();
+        slaineWindow.transform.SetParent (canvas.transform, false);
     }
-    
-    public void ShowPauseWindow() {
-        var pauseWindow = Instantiate(pauseWindowPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        var canvas = FindObjectOfType<Canvas>();
-        pauseWindow.transform.SetParent(canvas.transform, false);
+
+    public void ShowPauseWindow () {
+        _isGameOnPause = true;
+        var pauseWindow = Instantiate (pauseWindowPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+        var canvas = FindObjectOfType<Canvas> ();
+        pauseWindow.transform.SetParent (canvas.transform, false);
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,52 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     [SerializeField] private Animator anim;
     [SerializeField] private AudioSource audioSource;
 
     private bool touchBegan;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            anim.Play("PIxelNinjaPunchRight");
-            audioSource.Play();
+    private void Awake() {
+        GameController.Instance.ResumeGame ();
+    }
+
+    void Update () {
+
+        if (GameController.Instance.GameStoped()) {
+            return;
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            anim.Play("PixelNinjaPunchLeft");
-            audioSource.Play();
+        if (Input.GetKeyDown (KeyCode.D)) {
+            anim.Play ("PIxelNinjaPunchRight");
+            audioSource.Play ();
         }
 
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began && !touchBegan)
-            {
+        if (Input.GetKeyDown (KeyCode.A)) {
+            anim.Play ("PixelNinjaPunchLeft");
+            audioSource.Play ();
+        }
+
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch (0);
+            if (touch.phase == TouchPhase.Began && !touchBegan) {
                 touchBegan = true;
-                Debug.Log("Touch Position : " + touch.position);
+                Debug.Log ("Touch Position : " + touch.position);
 
-                if (touch.position.x > (Screen.width / 2))
-                {
-                    anim.Play("PIxelNinjaPunchRight");
-                    audioSource.Play();
-                }
-                else if (touch.position.x < (Screen.width / 2))
-                {
-                    anim.Play("PixelNinjaPunchLeft");
-                    audioSource.Play();
+                if (touch.position.x > (Screen.width / 2)) {
+                    anim.Play ("PIxelNinjaPunchRight");
+                    audioSource.Play ();
+                } else if (touch.position.x < (Screen.width / 2)) {
+                    anim.Play ("PixelNinjaPunchLeft");
+                    audioSource.Play ();
                 }
             }
 
-            if (touch.phase == TouchPhase.Ended)
-            {
+            if (touch.phase == TouchPhase.Ended) {
                 touchBegan = false;
             }
         }
-    }
+    } // update 
 }
