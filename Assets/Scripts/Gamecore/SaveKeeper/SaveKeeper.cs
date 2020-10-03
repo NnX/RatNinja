7f824ee35311af8503b4b20a4e3fe3b5af0b6528
NnX <hentis@ukr.net>
2020-10-04 01:24:02 +0300

    Implemented save/load system

---
+++ Assets/Scripts/Gamecore/SaveKeeper/SaveKeeper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SaveKeeper {
+
+    private SaveDataBox _saveDataBox;
+    private bool _isSaving;
+    private readonly string _savePath;
+
+    public SaveKeeper () {
+        _savePath = Application.persistentDataPath + "SaveBox.dat";
+    }
+
+    public int GetLevelKillsCount (int level) {
+        return _saveDataBox.LevelKills[level];
+    }
+    
+    public void SetLevelKillsCount (int level, int kills) {
+        if(_saveDataBox.LevelKills.ContainsKey(level)) {
+            
+            _saveDataBox.LevelKills[level] = kills;
+        } else {
+            _saveDataBox.LevelKills.Add(level, kills);
+        }
+    }
+
+    public int GetTotalKillsCount () {
+        return _saveDataBox.TotalKills;
+    }
+
+    public void IncreaseTotalKillCounter(int kills) {
+        _saveDataBox.TotalKills +=  kills;
+    }
+
+    public bool SaveDataBox () {
+        if (_isSaving) {
+            Debug.LogWarning ("Saving in progress");
+            return false;
+        }
+
+        _isSaving = true;
+        FileStream file = null;
+
+        try {
+            BinaryFormatter formatter = new BinaryFormatter ();
+            file = File.Create (_savePath);
+
+            if (_saveDataBox != null) {
+                SaveDataBox save = new SaveDataBox ();
+
+                save.TotalKills = _saveDataBox.TotalKills;
+                save.LevelKills = _saveDataBox.LevelKills;
+
+                formatter.Serialize (file, save);
+            }
+
+        } catch (System.Exception) {
+            Debug.LogError ("Error during saving");
+        } finally {
+            if (file != null) {
+                file.Close ();
+            }
+            _isSaving = false;
+            Debug.LogWarning ($"Saved data box, path = {_savePath}");
+        }
+
+        return true;
+    }
+
+    public bool LoadDataBox () {
+        FileStream fileStram = new FileStream(_savePath, FileMode.OpenOrCreate);
+
+        try {
+            if(fileStram == null || fileStram.Length == 0) {
+                _saveDataBox = new SaveDataBox();
+                _saveDataBox.LevelKills = new Dictionary<int, int>();
+                Debug.LogWarning ("Created new save file box");
+            } else {
+                 BinaryFormatter formatter = new BinaryFormatter ();
+                _saveDataBox = (SaveDataBox) formatter.Deserialize (fileStram);
+                Debug.LogWarning ("Loaded save file box");
+            }
+
+        } catch (Exception ex) {
+            Debug.LogError ($"Error during Loading = {ex}");
+        } finally {
+            if (fileStram != null) {
+                fileStram.Close ();
+            }
+        }
+        return true;
+    }
+
+}
