c9c928c3758b488438efbe328321d3c1a9bc2865
NnX <hentis@ukr.net>
2020-11-29 01:36:03 +0200

    implemented fx particle system on player damage received

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,6 +7,8 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private Slider healthBar;
     [SerializeField] private int healthValue = 100;
+    [SerializeField] private ParticleSystem particleSystemLeft;
+    [SerializeField] private ParticleSystem particleSystemRight;
 
     void Start() {
         healthBar.value = healthValue;
@@ -14,6 +16,8 @@ public class PlayerHealth : MonoBehaviour
 
     public void ApplyDamage(int damage) {
         healthValue -= damage;
+        particleSystemLeft.Play();
+        particleSystemRight.Play();
 
         if(healthValue <= 0) {
             healthValue = 0;
