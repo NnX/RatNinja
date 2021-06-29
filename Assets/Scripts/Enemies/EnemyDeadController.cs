7c7edc5f59bdbf385a54948df91c169f986dc6cd
NnX <hentis@ukr.net>
2021-06-29 17:55:12 +0300

    updated unity version, implementead dead body dotween animation, removed unused files

--- Assets/Scripts/Enemies/EnemyDeadController.cs
+++ Assets/Scripts/Enemies/EnemyDeadController.cs
@@ -1,36 +1,13 @@
-﻿using DG.Tweening;
-using System.Collections;
-using System.Threading.Tasks;
+﻿
 using UnityEngine;
-using UnityEngine.UI;
 
 public class EnemyDeadController : MonoBehaviour
 {
-    [SerializeField] Animator animator;
-    [SerializeField] Image deadbodyImage;
-    
-    public void PlayDeath(float scaleX) {
+    [SerializeField] private GameObject deadBody;
 
-        //PlayDeathAnimationAsync();
-        /*        if(scaleX > 0) {
-                    animator.Play("RatDeadLeftToRight");
-
-                } else {
-                    animator.Play("RatDeadRightToLeft");
-                }
-
-                StartCoroutine(ClearDeadBody());*/
-    }
-
-    private IEnumerator ClearDeadBody()
+    public GameObject DeadbodyPrefab()
     {
-        yield return new WaitForSeconds(1f);
-        Destroy(this.gameObject);
+        return deadBody;
     }
 
-    public async void PlayDeathAnimationAsync()
-    {
-       await Task.Run(() => transform.DOMove(new Vector2(1, 1), 2f));
-        Destroy(this);
-    }
 }
--- Assets/Scripts/Player/WeaponController.cs
+++ Assets/Scripts/Player/WeaponController.cs
@@ -1,36 +1,52 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class WeaponController : MonoBehaviour
 {
 
-    [SerializeField] Text points; 
+    [SerializeField] Text points;
     [SerializeField] private GameObject damagePoint;
     [SerializeField] private GameObject ratDeadPrefab;
 
 
-    private void OnTriggerEnter2D(Collider2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
 
-        if (collision.tag == "Enemy") {
-            int pts = System.Int32.Parse(points.text);
+        if (collision.tag == "Enemy")
+        {
+            int pts = int.Parse(points.text);
             pts++;
             GameController.Instance.levelKills = pts;
             points.text = pts.ToString();
 
-            var enemy = Instantiate(ratDeadPrefab, collision.gameObject.transform.localPosition, Quaternion.identity);
+            EnemyDeadController deadController = collision.gameObject.GetComponent<EnemyDeadController>();
 
-            float scaleX = collision.gameObject.transform.localScale.x;
+            var body = Instantiate(deadController.DeadbodyPrefab(), collision.gameObject.transform.localPosition, Quaternion.identity);
+
             Destroy(collision.gameObject);
-            enemy.GetComponent<EnemyDeadController>().PlayDeath(scaleX);
+            PlayDeathAnimation(body);
         }
     }
 
-    void AllowDamageCollision() {
+    void AllowDamageCollision()
+    {
         damagePoint.SetActive(true);
     }
 
 
-    void DenyDamageCollision() {
+    void DenyDamageCollision()
+    {
         damagePoint.SetActive(false);
+    }
+
+    public async void PlayDeathAnimation(GameObject body)
+    {
+        Vector2 flyDirection = new Vector2(25, 20);
+        body.transform.DOMove(flyDirection, 2f);
+
+        await Task.Delay(2500);
+        Destroy(body);
     }
 }
