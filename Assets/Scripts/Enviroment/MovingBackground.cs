c26db874c363c35c661cc3f306ae9e82436ad1ce
Vitaly <hentis@ukr.net>
2021-12-23 23:44:21 +0200

    implemented infinite backround scroll, platform prefabs

--- Assets/Scripts/Enviroment/MovingBackground.cs
+++ Assets/Scripts/Enviroment/MovingBackground.cs
@@ -1,22 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MovingBackground : MonoBehaviour
 {
+    [SerializeField] private float backgroundWidth;
+    [SerializeField] private float movingSpeed;
+    [SerializeField] private Transform[] backgrounds;
+    private Vector3 _bgPosition;
 
-    [SerializeField] Transform background;
-    [SerializeField] float movingSpeed;
-
-    void Update()
+    private void Update()
     {
         MoveGround();
     }
 
     private void MoveGround()
     {
-        var ground1Position = new Vector3(background.position.x, background.position.y, background.position.z);
-        ground1Position.x -= movingSpeed * Time.deltaTime;
-        background.position = ground1Position;
+        foreach (var background in backgrounds)
+        {
+            var position = background.position;
+            var ground1Position = new Vector3(position.x, position.y, position.z);
+            ground1Position.x -= movingSpeed * Time.deltaTime;
+            position = ground1Position;
+            background.position = position;
+            if (background.position.x < -(backgroundWidth + 5))
+            {
+                var maxPositionX = backgrounds.Max(x => x.position.x);
+                _bgPosition.x = maxPositionX + backgroundWidth;
+                _bgPosition.y = background.position.y;
+                _bgPosition.z = background.position.z;
+                background.position = _bgPosition;
+            }
+        } 
     }
 }
